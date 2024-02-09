41b81c35a16881650a5282e93b56aa552d0562b7
Ayaulym Orazbayeva <sendaiyazzz@gmail.com>
2024-02-10 00:04:52 +0600

    update for type parameter

--- src/main/java/repositories/interfaces/IQuestionRepository.java
+++ src/main/java/repositories/interfaces/IQuestionRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface IQuestionRepository {
     Question getQuestion(int id, String subject);
     List<Question> getAllQuestions(String subject);
+
+    List<Question> getAllQuestions(String subject, String type);
 }

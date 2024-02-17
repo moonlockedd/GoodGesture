7122db33749b50c6f87468129dfb7dc98dd65283
Ayaulym Orazbayeva <sendaiyazzz@gmail.com>
2024-02-17 10:39:32 +0600

    Implementing the Program class with Lombok

--- src/main/java/models/Program.java
+++ src/main/java/models/Program.java
@@ -1,4 +1,30 @@
 package models;
+//import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 
 public class Program {
+    @Getter @Setter private int id;
+    @Getter @Setter String programName;
+    @Getter @Setter String uniName;
+    @Getter @Setter int minScore;
+
+   public Program(int id, String programName, String uniName, int minScore){
+       this.id = id;
+       this.programName= programName;
+       this.uniName= uniName;
+       this.minScore = minScore;
+   }
+
+   @Override
+    public String toString(){
+       return "Program{"+
+               "id=" + id +
+               ", name='" + programName + '\'' +
+               ", university name='" + uniName + '\'' +
+               ", minimum score=" + minScore +
+               '}';
+   }
+
 }

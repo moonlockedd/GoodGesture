9c0b68c1cf4c55e0eeac72cf108a604237faa8fc
Nurali <231087@astanait.edu.kz>
2024-02-17 10:12:39 +0600

    Add Subject class

---
+++ src/main/java/models/Subject.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter @AllArgsConstructor
+public class Subject {
+    private String name;
+    private int score;
+
+    @Override
+    public String toString() {
+        return getName() + ": " + getScore();
+    }
+}

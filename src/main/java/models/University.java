fb661e774957a6e7aaba3828e8c8e429ccadac09
Ayaulym Orazbayeva <sendaiyazzz@gmail.com>
2024-02-18 13:29:24 +0600

    Implementing the University class with Lombok

---
+++ src/main/java/models/University.java
@@ -0,0 +1,26 @@
+package models;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter @Setter
+
+public class University{
+    private int id;
+    private String universityName;
+    private List<Program> programs;
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id=" + id +
+                ", universityName='" + universityName + '\'' +
+                ", programs=" + programs +
+                '}';
+    }
+
+}
+
+

49114b1e02ec58bd9f4199838e3a2bc49ab49ae4
Nurali <231087@astanait.edu.kz>
2024-02-28 01:23:11 +0600

    Rework Program class fields

--- src/main/java/models/Program.java
+++ src/main/java/models/Program.java
@@ -7,24 +7,24 @@ import lombok.Setter;
 @Getter @Setter
 public final class Program {
     private int id;
-    private String programName;
-    private int minScore;
+    private String name;
+    private int minimumScore;
     private String[] electedSubjectNames;
 
-    public Program(String programName, int minScore, String[] electedSubjectNames) {
-        this.programName = programName;
-        this.minScore = minScore;
+    public Program(String name, int minimumScore, String[] electedSubjectNames) {
+        this.name = name;
+        this.minimumScore = minimumScore;
         this.electedSubjectNames = electedSubjectNames;
     }
 
     @Override
     public String toString(){
        return "Program #" + getId() + ":\n" +
-               getProgramName() + "\n" +
+               getName() + "\n" +
                "Elected subjects: " +
                getElectedSubjectNames()[0] + ", " +
                getElectedSubjectNames()[1] + "\n" +
-               "Minimum score: " + getMinScore();
+               "Minimum score: " + getMinimumScore();
    }
 
 }
